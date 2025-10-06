4b8125979451bf617977c89334480c9c72eadfdd
Paul Brandenburg <kuba.brandenburg@poczta.fm>
2025-10-06 06:40:35 +0000

    Ulepszenie path handlingu, dodanie HTML'a i rozbicie kodu na kilka plików

--- index.js
+++ index.js
@@ -1,20 +1,17 @@
-import { createServer } from 'node:http';
+import { createServer } from "node:http";
 import { URL } from "node:url";
+import { handlePath } from "./src/path_handlers.js";
 
 // Create a HTTP server
 const server = createServer((req, res) => {
   const request_url = new URL(`http://${host}${req.url}`);
-  const path = request_url.pathname;
-  console.log(`Request: ${req.method} ${path}`);
+  console.log(`Request: ${req.method} ${request_url.pathname}`);
 
-  if (path === "/" && req.method === 'GET') {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('hello world!\n');
-  }
+  handlePath(request_url.pathname, req, res);
 
   if (!res.writableEnded) {
-    res.writeHead(404, { 'Content-Type': 'text/plain' });
-    res.end('Site not found!\n');
+    res.writeHead(404, { "Content-Type": "text/plain" });
+    res.end("Site not found!\n");
   }
 });
 
@@ -23,5 +20,5 @@ const host = "localhost";
 
 // Start the server
 server.listen(port, host, () => {
-    console.log(`Server listening on http://${host}:${port}`);
+  console.log(`Server listening on http://${host}:${port}`);
 });
